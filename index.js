04e5f88c61f8175b657072f640378d0f7318ad4f
jjaeho0415 <jjh0773@naver.com>
2024-03-20 12:00:07 +0900

    challenge13

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ import {AppRegistry, Platform} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import PushNotification from 'react-native-push-notification';
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
 
 PushNotification.configure({
   onNotification: function (notification) {
     console.log('Notification', notification);
+
+    notification.finish(PushNotificationIOS.FetchResult.NoData);
   },
   popInitialNotification: true,
   requestPermissions: Platform.OS === 'ios',
